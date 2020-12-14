cd3876c592713946f03fb46d395f1485e26ee5fa
chriskhng <christopher.ng@yale.edu>
2020-12-14 15:22:34 -0500

    FEAT: asSeq2Int.py opens the FASTA

--- asSeq2Int.py
+++ asSeq2Int.py
@@ -8,7 +8,7 @@
 #### TODO put Part 1 function below
 #### Part 1 reformat the FASTA file into list of list for Part 2
 #### It first opens the file
-
+with open('ct_border.txt') as ct_border_file:
 #### Splits the file into a list using "*/n>" as the delimiter
 #### File can close now
 
