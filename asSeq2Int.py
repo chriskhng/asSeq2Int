90aad86d67d131007fe54126dbb999deaeff3c58
chriskhng <christopher.ng@yale.edu>
2020-12-14 18:21:06 -0500

    FEAT: asSeq2Int.py tidies up artifacts from reformating the FASTA

--- asSeq2Int.py
+++ asSeq2Int.py
@@ -9,6 +9,7 @@ lst_comma_raw_FASTA = []
 lst_trimmed_FASTA = []
 proteinSeqOnly = []
 lst_FASTAentry = []
+lst_masterFASTA = []
 
 #### TODO put Part 1 function below
 #### Part 1 reformat the FASTA file into list of list for Part 2
@@ -21,45 +22,30 @@ lst_rawFASTA = FASTA_data.split('*\n>')
 
 for FASTAprotein in lst_rawFASTA:
 #### we pull out the protein name from each FASTA entry
-    #print(FASTAprotein)
-    #print(FASTAprotein.find(','))
     FASTAproteinNames = FASTAprotein[0: FASTAprotein.find(',')]
-    #print(FASTAproteinNames)
     proteinNamesSep = FASTAproteinNames.split(" ")
-
 
 #### now we put out the protein sequence from the FASTA list
 #### replaces all \n with ,
     FASTAprotein_comma = FASTAprotein.replace("\n", ", ")
 #### replaces all '\",' with \&
     for_protein_seqjoin = FASTAprotein_comma.replace('\",', '*$')
-    #for_protein_seqjoin = for_protein_seqjoin + "*"
-    #print(for_protein_seqjoin.find("*$"))
-    #print(for_protein_seqjoin[for_protein_seqjoin.find("*$")+3])
 #### Saves sequence data into proteinSeqOnly starting from index of (*$ +3) to the last index
     proteinSeqOnly = for_protein_seqjoin[ for_protein_seqjoin.find("*$")+3:]
-    #print(proteinSeqOnly)
     proteinSeqFinal = proteinSeqOnly.replace(', ', '')
-    #print(proteinSeqFinal)
     lst_FASTAentry = FASTAproteinNames.split(' ')
     lst_FASTAentry.append (proteinSeqFinal)
-    print(lst_FASTAentry)
+    lst_masterFASTA.append (lst_FASTAentry)
+
+# lst_masterFASTA is trimmed to rid of the > and * and the start and end (artifacts from the above reformatting)
+(lst_masterFASTA[0])[0] = (lst_masterFASTA[0])[0].replace('>', '')
+(lst_masterFASTA[-1])[-1] = (lst_masterFASTA[-1])[-1].replace('*', '')
+print(lst_masterFASTA)
+#print(FASTAmasterIndex0)
+#lst_masterFASTA_untrimmed[1[1]].replace('>', '')
+#print(lst_masterFASTA_untrimmed)
 
 
-
-#### splits those items by ", " into list
-    FASTAprotein_comma_split = FASTAprotein_comma.split(', ')
-#### delete most unused items
-    del FASTAprotein_comma_split[1:5]
-    #print(FASTAprotein_comma_split)
-    #print(for_protein_seqjoin)
-    #print(unjoined_proteinseq)
-    #for letter in FASTAprotein_comma_split:
-
-#### joins the protein sequence into one item
-    FASTAprotein_seqjoin = [FASTAprotein_comma_split[0], ''.join(FASTAprotein_comma_split[1:len(FASTAprotein_comma_split)])]
-    lst_comma_raw_FASTA.append(FASTAprotein_comma_split)
-    lst_trimmed_FASTA.append(FASTAprotein_seqjoin)
 
 
 
