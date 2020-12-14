73b829e1ecc648072472bed091730d0ff2061640
chriskhng <christopher.ng@yale.edu>
2020-12-14 15:54:20 -0500

    FEAT: asSeq2Int.py now splits FASTA_data by *\n>

--- asSeq2Int.py
+++ asSeq2Int.py
@@ -3,13 +3,24 @@
 ## -
 ## - Created by Chris Ng 20201214
 
-
+## - #### Initialize empty lists
+lst_rawFASTA = []
 
 #### TODO put Part 1 function below
 #### Part 1 reformat the FASTA file into list of list for Part 2
 #### It first opens the file
-with open('ct_border.txt') as ct_border_file:
+with open('dummy1.FASTA') as FASTA_file:
 #### Splits the file into a list using "*/n>" as the delimiter
+    FASTA_data = FASTA_file.read()
+#print(FASTA_data)
+lst_rawFASTA = FASTA_data.split('*\n>')
+
+
+   # for line in FASTA_file
+    #lst_rawFASTA = FASTA_file.split('*/n>')
+    #for line in FASTA_file:
+     #   print(line)
+
 #### File can close now
 
 #### "reverse compliment, " is removed
@@ -17,3 +28,6 @@ with open('ct_border.txt') as ct_border_file:
 #### "/n" is replaced with ", " for all items in list
 
 #### items in list is split into a list with ", " as the delimiter.
+
+#### TESTING lines
+print(lst_rawFASTA)
