1c244ceb0968f59f866fda25f0baf2827fa71b1c
chriskhng <christopher.ng@yale.edu>
2020-12-14 15:10:16 -0500

    DOC: to asSeq2Int.py added TODO for part 1

--- asSeq2Int.py
+++ asSeq2Int.py
@@ -5,3 +5,15 @@
 
 
 
+#### TODO put Part 1 function below
+#### Part 1 reformat the FASTA file into list of list for Part 2
+#### It first opens the file
+
+#### Splits the file into a list using "*/n>" as the delimiter
+#### File can close now
+
+#### "reverse compliment, " is removed
+
+#### "/n" is replaced with ", " for all items in list
+
+#### items in list is split into a list with ", " as the delimiter.
